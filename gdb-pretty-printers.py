2afd1c30d6f25205e3776eb0103f011e3905d46d
Bastien Bouclet <bastien.bouclet@gmail.com>
2015-08-14 21:24:36 +0200

    GDB: Add a pretty printer for Math::Vector3d

--- gdb-pretty-printers.py
+++ gdb-pretty-printers.py
@@ -49,8 +49,40 @@ class CommonArrayPrinter:
     def display_hint():
         return 'array'
 
+class MathVector3dPrinter:
+    """Print a Math::Vector3d"""
+
+    class _Iterator:
+        def __init__(self, storage):
+            self.item = storage
+            self.size = 3
+            self.count = 0
+            self.names = ('x', 'y', 'z')
+
+        def __iter__(self):
+            return self
+
+        def next(self):
+            count = self.count
+            self.count += 1
+            if self.count > self.size:
+                raise StopIteration
+
+            return '[%s]' % self.names[count], self.item[count]
+
+    def __init__(self, val):
+        self.val = val
+
+    def children(self):
+        return self._Iterator(self.val['_values'])
+
+    @staticmethod
+    def display_hint():
+        return 'array'
+
 printers = gdb.printing.RegexpCollectionPrettyPrinter('scummvm')
 printers.add_printer('Common::String', '^Common::String$', CommonStringPrinter)
 printers.add_printer('Common::Array', '^Common::Array<.*>$', CommonArrayPrinter)
+printers.add_printer('Math::Vector3d', '^Math::Matrix<3, 1>$', MathVector3dPrinter)
 gdb.printing.register_pretty_printer(None, printers, True)
 
